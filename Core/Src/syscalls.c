e7757d0edfa1db32a309946188b16bfc448519e2
Jeremy Overesch <ackbar9722@gmail.com>
2025-10-27 11:33:43 -0500

    Add ability for printf() to the debugger.

--- Core/Src/syscalls.c
+++ Core/Src/syscalls.c
@@ -30,6 +30,8 @@
 #include <sys/time.h>
 #include <sys/times.h>
 
+#include "stm32h7xx.h"
+
 
 /* Variables */
 extern int __io_putchar(int ch) __attribute__((weak));
@@ -84,7 +86,7 @@ __attribute__((weak)) int _write(int file, char *ptr, int len)
 
   for (DataIdx = 0; DataIdx < len; DataIdx++)
   {
-    __io_putchar(*ptr++);
+    ITM_SendChar(*ptr++);
   }
   return len;
 }
